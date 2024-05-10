d723e7a2443681e1171aa1c3892a4fb7706cad03
Sai Prafful Indirala <prafful@builtbypixel.com>
2024-05-10 10:31:54 +0100

    Header logo changes

--- src/navigation/BottomTab.tsx
+++ src/navigation/BottomTab.tsx
@@ -5,6 +5,8 @@ import MyBottomTabs from '@src/components/MyBottomTabs';
 import CreatePostScreen from '@src/screens/CreatePostScreen';
 import Icon from 'react-native-vector-icons/Feather';
 
+import { Image, View } from '@gluestack-ui/themed';
+
 import AccountNavigator from './stacks/AccountNavigator';
 import HomeNavigator from './stacks/HomeNavigator';
 import NotificationNavigator from './stacks/NotificationNavigator';
@@ -34,7 +36,27 @@ const BottomTabNavigator = () => {
           headerTintColor: '#fff',
           headerShadowVisible: false, // applied here
           headerBackTitleVisible: false,
-          headerTitle: 'PanoramaPal',
+
+          headerTitle: () => (
+            <View>
+              <Image
+                w={150}
+                h={50}
+                source={require('../../assets/images/logo.png')}
+                // backgroundColor="white"
+                borderRadius={10}
+                zIndex={1}
+              />
+              <View
+                backgroundColor="white"
+                w={150}
+                h={12}
+                position="absolute"
+                bottom={0}
+                borderRadius={5}
+              />
+            </View>
+          ),
         })}
       />
 
