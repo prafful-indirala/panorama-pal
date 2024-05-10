33c25a3cfcfa7fbb573502ed22c04820fd722010
Sai Prafful Indirala <prafful@builtbypixel.com>
2024-05-10 09:55:54 +0100

    Post card changes

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Feather from 'react-native-vector-icons/Feather';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Octicons from 'react-native-vector-icons/Octicons';
@@ -12,18 +13,29 @@ import {
   VStack,
 } from '@gluestack-ui/themed';
 
+import CurveSvg from '../CurveSvg';
+
 const PostHeader = () => (
   <HStack justifyContent="space-between" alignItems="center">
     <HStack gap={4} alignItems="center">
-      <Image
-        source={require('../../../assets/images/user.png')}
-        h={25}
-        w={25}
+      <View borderWidth={1.8} borderColor="$secondary" borderRadius={9} p={1}>
+        <Image
+          source={require('../../../assets/images/users/user1.jpg')}
+          h={25}
+          w={25}
+          borderRadius={6}
+        />
+      </View>
+      <Text fontWeight="bold" fontSize="$xs">
+        nicki
+      </Text>
+      <MaterialCommunityIcons
+        name="check-decagram"
+        size={13}
+        color="$secondary"
       />
-
-      <Text>User 1</Text>
     </HStack>
-    <Text>...</Text>
+    <Feather name="more-horizontal" size={22} color="grey" />
   </HStack>
 );
 const PostContent = () => (
@@ -40,24 +52,44 @@ const PostContent = () => (
 );
 
 const PostFooter = () => (
-  <HStack justifyContent="space-between" alignItems="center">
-    <HStack gap={10} alignItems="center">
-      <Octicons name="heart-fill" size={20} color="red" />
-      {/* <Octicons name="heart" size={20} /> */}
-      <Fontisto name="commenting" size={20} />
+  <VStack gap={10}>
+    <HStack justifyContent="space-between" alignItems="center">
+      <HStack gap={10} alignItems="center">
+        <Octicons name="heart-fill" size={20} color="red" />
+        {/* <Octicons name="heart" size={20} /> */}
+        <Fontisto name="commenting" size={20} />
 
-      <Fontisto name="share-a" size={20} />
+        <Fontisto name="share-a" size={20} />
+      </HStack>
+      <MaterialCommunityIcons name="bookmark-multiple-outline" size={20} />
     </HStack>
-    <MaterialCommunityIcons name="bookmark-multiple-outline" size={20} />
-  </HStack>
+    <HStack gap={4}>
+      <Text fontSize="$sm">
+        <Text fontWeight="bold" fontSize="$sm">
+          Chris
+        </Text>{' '}
+        Lost in the ethereal beauty of nature's masterpiece 🌿✨ #NatureLover
+        #ScenicViews
+      </Text>
+    </HStack>
+  </VStack>
 );
 
 export default function PostCard() {
   return (
-    <VStack px={30} py={15} gap={15}>
-      <PostHeader />
-      <PostContent />
-      <PostFooter />
-    </VStack>
+    <View backgroundColor="transparent" m={10}>
+      <View
+        backgroundColor="$white"
+        borderTopLeftRadius={20}
+        borderTopRightRadius={20}
+      >
+        <VStack p={20} gap={15}>
+          <PostHeader />
+          <PostContent />
+          <PostFooter />
+        </VStack>
+      </View>
+      <CurveSvg rotate="180deg" width="390" height="60" bgColor="white" />
+    </View>
   );
 }
