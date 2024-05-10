dd9a405543b83656b0619aa0909c53945ed00141
Sai Prafful Indirala <prafful@builtbypixel.com>
2024-05-10 09:55:32 +0100

    static user data changes

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -2,64 +2,76 @@ import React from 'react';
 
 import { Image, ScrollView, Text, View, VStack } from '@gluestack-ui/themed';
 
+import CurveSvg from './CurveSvg';
+
 const userData = [
   {
     id: 1,
-    name: 'User 1',
+    name: 'Nicki',
     image: require('../../assets/images/user.png'),
   },
   {
     id: 2,
-    name: 'User 2',
+    name: 'Rogar',
     image: require('../../assets/images/user.png'),
   },
   {
     id: 3,
-    name: 'User 3',
+    name: 'Sherry',
     image: require('../../assets/images/user.png'),
   },
   {
     id: 4,
-    name: 'User 4',
+    name: 'Alion',
     image: require('../../assets/images/user.png'),
   },
   {
     id: 5,
-    name: 'User 5',
+    name: 'Philip',
     image: require('../../assets/images/user.png'),
   },
   {
     id: 6,
-    name: 'User 6',
+    name: 'John',
     image: require('../../assets/images/user.png'),
   },
   {
     id: 7,
-    name: 'User 7',
+    name: 'Doe',
     image: require('../../assets/images/user.png'),
   },
 ];
 
 export default function Highlights() {
   return (
-    <View pt={15} px={10} backgroundColor="$primary">
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {userData.map(user => (
-          <VStack m={5} key={user?.id} alignItems="center">
-            <Image
-              source={require('../../assets/images/highlight.jpg')}
-              h={50}
-              w={50}
-              rounded={10}
-              //   borderWidth={2}
-              borderTopLeftWidth={5}
-            />
-            <Text fontSize={10} color="$white">
-              {user?.name}
-            </Text>
-          </VStack>
-        ))}
-      </ScrollView>
+    <View backgroundColor="transparent">
+      <View pt={15} px={10} backgroundColor="$primary">
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {userData.map(user => (
+            <VStack m={5} key={user?.id} alignItems="center">
+              <View
+                borderWidth={1.8}
+                rounded={15}
+                p={1}
+                borderColor="$secondary"
+              >
+                <Image
+                  source={require('../../assets/images/highlight.jpg')}
+                  h={50}
+                  w={50}
+                  rounded={15}
+                />
+              </View>
+
+              <Text fontSize={10} color="$white">
+                {user?.name}
+              </Text>
+            </VStack>
+          ))}
+        </ScrollView>
+      </View>
+
+      <CurveSvg rotate="180deg" width="400" height="80" bgColor="#253238" />
     </View>
   );
 }
